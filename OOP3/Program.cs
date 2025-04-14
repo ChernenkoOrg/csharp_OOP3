560e34354d681025926028d31ecc2fe2a0ca35ef
cry4alone <chernenkoa04@gmail.com>
2025-04-14 19:58:37 +0700

    1st step

--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -10,7 +10,33 @@ namespace OOP3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Игра \"Угадай число\" \n Чтобы начать игру введите свое число");
+            int input = int.Parse(Console.ReadLine());
+            int tries = 2;
 
+            Random random = new Random();
+            int randomNumber = random.Next(0, 11);
+
+            Console.WriteLine(randomNumber);
+
+            while (tries-- > 0) {
+                if (input == randomNumber)
+                {
+                    Console.WriteLine("Your super puper mega ultra");
+                    return;
+                }
+                else if (input > randomNumber)
+                {
+                    Console.WriteLine("Загаданное число меньше");
+                    input = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Загаданное число больше");
+                    input = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Game over");
         }
     }
 }
